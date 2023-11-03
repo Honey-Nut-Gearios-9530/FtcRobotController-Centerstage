410a2efe9ba8aa874843832b566d21b96e0bfeee
Justin McCubbin <jmccubbin25@gunston.org>
2023-11-03 16:33:31 -0400

    add button for direction swap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/old/DemonstrationTeleOpPowerplayKt.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/old/DemonstrationTeleOpPowerplayKt.kt
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.Gamepad
 import org.firstinspires.ftc.robotcore.external.matrices.GeneralMatrixF
 import kotlin.math.absoluteValue
 
@@ -11,12 +12,14 @@ import kotlin.math.absoluteValue
 @Suppress("unused")
 //@Disabled
 class DemonstrationTeleOpPowerplayKt : LinearOpMode() {
-    private val speedMultiplier = 1
+    private var speedMultiplier = 1
     private lateinit var leftFront: DcMotor
     private lateinit var rightFront: DcMotor
     private lateinit var leftBack: DcMotor
     private lateinit var rightBack: DcMotor
-    private var motors = arrayOf(leftFront, rightFront, leftBack, rightBack)
+    private lateinit var motors: Array<DcMotor>
+    private var currentGamepad = Gamepad()
+    private var pastGamepad = Gamepad()
 
     override fun runOpMode() {
         initialize()
@@ -26,24 +29,37 @@ class DemonstrationTeleOpPowerplayKt : LinearOpMode() {
         telemetry.update()
 
         while (opModeIsActive()) {
+            try {
+                pastGamepad.copy(currentGamepad)
+                currentGamepad.copy(gamepad1)
+            } catch (ignored: Exception) {
+            }
+
+
+            if (currentGamepad.a && !pastGamepad.a) speedMultiplier *= -1
+
             // calculate amount of rotation
-            val rotate = gamepad2.left_trigger + -gamepad2.right_trigger
+            val rotate = (gamepad1.left_trigger + -gamepad1.right_trigger) * speedMultiplier
 
             // matrix that hold the directions the wheels need to go
             // for movement in the x, y and rotational axes
-            val directionMatrix = GeneralMatrixF(4, 3, floatArrayOf(
+            val directionMatrix = GeneralMatrixF(
+                4, 3, floatArrayOf(
                     1f, -1f, -1f,
                     1f, 1f, 1f,
                     1f, 1f, -1f,
                     1f, -1f, 1f
-            ))
+                )
+            )
 
             // matrix that hold the state of controller input that is translated to wheel speeds
-            val inputMatrix = GeneralMatrixF(3, 1, floatArrayOf(
+            val inputMatrix = GeneralMatrixF(
+                3, 1, floatArrayOf(
                     -gamepad1.right_stick_y,
-                    -gamepad2.right_stick_x,
+                    -gamepad1.right_stick_x,
                     rotate
-            ))
+                )
+            )
 
             // 4 length float that holds the speeds for each wheel
             // calculated by summing the weights of the wheel movement
@@ -52,13 +68,13 @@ class DemonstrationTeleOpPowerplayKt : LinearOpMode() {
 
             // normalize the wheel speeds to within 0..1 and apply the new speeds
             val maxSpeed = wheelSpeeds.maxOfOrNull { it.absoluteValue }!!
-            wheelSpeeds.map { it / maxSpeed }
-                    .zip(motors)
-                    .forEach { it.second.power = it.first.toDouble() * speedMultiplier }
-
-            telemetry.addLine("Stopping")
-            telemetry.update()
+            wheelSpeeds.map { if (maxSpeed > 1) it / maxSpeed else it }
+                .zip(motors)
+                .forEach { it.second.power = it.first.toDouble() * speedMultiplier }
         }
+
+        telemetry.addLine("Stopping")
+        telemetry.update()
     }
 
     private fun initialize() {
@@ -69,6 +85,8 @@ class DemonstrationTeleOpPowerplayKt : LinearOpMode() {
         rightFront = hardwareMap.dcMotor["rfdrive"]
         leftBack = hardwareMap.dcMotor["lbdrive"]
         rightBack = hardwareMap.dcMotor["rbdrive"]
+
+        motors = arrayOf(leftFront, rightFront, leftBack, rightBack)
 
         // faster than using encoders
         motors.forEach { it.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER }
